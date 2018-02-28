3cebbd45f882e629d3b2c364c4182e77a316b269
Cindy Le <36822198+DotCindy@users.noreply.github.com>
2018-02-28 18:36:04 -0500

    Create at_home.cpp

---
+++ W1/at_home.cpp
@@ -0,0 +1,13 @@
+/* -------------------------------------------
+Name:                   Cindy Le
+Email:                  cle15@myseneca.ca
+Date:                   Sep 22, 2017
+---------------------------------------------- */
+#include <stdio.h>
+
+int main(void) {
+	printf("******************************\n");
+	printf("** Welcome to C Programming **\n");
+	printf("******************************\n");
+	return 0;
+}
