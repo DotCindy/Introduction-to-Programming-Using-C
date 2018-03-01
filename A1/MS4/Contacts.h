6f0c12eae95e5d2970581285f236276f02889144
Cindy Le <36822198+DotCindy@users.noreply.github.com>
2018-02-28 19:13:02 -0500

    Create Contacts.h

---
+++ A1/MS4/Contacts.h
@@ -0,0 +1,59 @@
+/* -------------------------------------------
+Name:           Cindy Le
+Email:          cle15@myseneca.ca
+Date:           Dec 12, 2017
+Workshop:	      Milestone 4
+Description:	  Contact System
+---------------------------------------------- */
+// Structure type Name declaration
+struct Name {
+	char firstName[31];
+	char middleInitial[7];
+	char lastName[36];
+};
+
+// Structure type Address declaration
+// Place your code here...
+struct Address {
+	int streetNumber;
+	char street[41];
+	int apartmentNumber;
+	char postalCode[8];
+	char city[41];
+};
+
+
+// Structure type Numbers declaration
+// Place your code here...
+struct Numbers {
+	char cell[21];
+	char home[21];
+	char business[21];
+};
+
+// Structure type Contact declaration
+// Place your code here...
+struct Contact {
+	struct Name name;
+	struct Address address;
+	struct Numbers numbers;
+};
+
+//------------------------------------------------------
+// Function Prototypes
+//------------------------------------------------------
+
+// ====== Milestone 4 =======
+
+// Get and store from standard input the values for Name
+// Place your code here...
+void getName(struct Name *name);
+
+
+// Get and store from standard input the values for Address
+// Place your code here...
+void getAddress(struct Address *address);
+
+// Get and store from standard input the values for Numbers
+// Place your code here...
+void getNumbers(struct Numbers *numbers);
